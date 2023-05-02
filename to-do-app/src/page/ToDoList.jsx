ebc2298d3d9fba28094cb8571fd62e0a4667d253
Lakshmi <lak2192@gmail.com>
2023-05-02 11:10:05 +1000

    search bar added

--- to-do-app/src/page/ToDoList.jsx
+++ to-do-app/src/page/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Task from './Task';
 import { useState } from "react";
 import TextField from '@mui/material/TextField';
@@ -10,6 +10,9 @@ import AddIcon from '@mui/icons-material/Add';
 const TodoList = () => {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredTasks, setFilteredTask] = useState([])
+
   
     const handleTaskChange = (event) => {
       setNewTask(event.target.value);
@@ -17,33 +20,64 @@ const TodoList = () => {
   
     const handleAddTask = () => {
       if (Object.keys(newTask).length>0) {
-        setTasks([...tasks, { description: newTask, isDone: false }]);
+        setTasks([...tasks, { description: newTask, isDone: false, id:Math.random() }]);
         setNewTask('');
       }
     };
   
-    const handleTaskDelete = (index) => {
-      const updatedTasks = tasks.filter((_, i) => i !== index);
+    const handleTaskDelete = (taskId) => {
+      const updatedTasks = tasks.filter((task, i) => task.id !== taskId);
       setTasks(updatedTasks);
     };
 
     
-  const handleTaskDone = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks[index].isDone = !updatedTasks[index].isDone;
+  const handleTaskDone = (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, isDone: !task.isDone };
+        } else {
+          return task;
+        }
+      });
     setTasks(updatedTasks);
   };
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(()=>{
+    let newTaskList = tasks
+    if(searchTerm !== '')
+     newTaskList = tasks.filter((task) =>
+    task.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTask(newTaskList)
+  },[tasks, searchTerm])
+
+ 
   
     return (
       <>
+      <Stack direction="column" spacing={2} >
          <Stack direction="row" spacing={2}  justifyContent="center">
-         <TextField type="text"  value={newTask} onChange={handleTaskChange} size="small"/>
+         <TextField value={newTask} onChange={handleTaskChange} size="small"/>
          <Button  variant="contained" onClick={handleAddTask}  startIcon={<AddIcon />} size="small">Add New Task</Button>
          </Stack>
+         <Stack direction="row" justifyContent="center">
+         <TextField
+          type="search"
+          placeholder="Search tasks"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+          size="small"
+        />
+        </Stack>
+        </Stack>
         <ul>
-          {tasks.length > 0 ? tasks.map((task, index) => (
-            <li key={index}>
-              <Task description={task.description} id={index} onDelete={handleTaskDelete} isDone={task.isDone}
+          {filteredTasks.length > 0 ? filteredTasks.map((task, index) => (
+            <li key={task.id}>
+              <Task description={task.description} id={task.id} onDelete={handleTaskDelete} isDone={task.isDone}
               onDone={handleTaskDone}/>
             </li>
           )): <div>No Task</div>}
